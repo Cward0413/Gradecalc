3feebff7c7a0feb847567031b6924112dac3ecde
Cward0413 <Conor.Ward94@gmail.com>
2020-12-02 21:03:59 -0600

    Completing Lab

--- main.js
+++ main.js
@@ -3,59 +3,92 @@ $(document).ready(
 
         //add event handlers
         $("#calc").click(calcGrade);
+        $("#sName").click(sortName);
+        $("#sGrade").click(sortGrade);
         //all other functions (program logic)
-        var studentGrade = {};
+        var sArray = [];
 
         function calcGrade(){
             event.preventDefault()
-            studentGrade.fName = $("#fName").val();
-            studentGrade.lName = $("#lName").val();
-            var studentPoints = $("#points").val();
-            var maxPoints = $("#posPoints").val();
 
-            studentGrade.percent = ((studentPoints/maxPoints) * 100).toFixed(2);
+            //get user input
+            var student = {};
+            student.Name = $("#fName").val() + " " + $("#lName").val();
+            student.Percent = ((($("#points").val())/($("#posPoints").val())) * 100).toFixed(2);
 
-            if (studentGrade["percent" ] >= 93){
-                studentGrade.letterGrade = "A";
-            }
-            else if (studentGrade["percent"] >= 90){
-                studentGrade.letterGrade = "A-";
-            }
-            else if (studentGrade["percent"] >= 87){
-                studentGrade.letterGrade = "B+";
-            }
-            else if (studentGrade["percent"] >= 83){
-                studentGrade.letterGrade = "B";
-            }
-            else if (studentGrade["percent"] >= 80){
-                studentGrade.letterGrade = "B-";
-            }
-            else if (studentGrade["percent"] >= 77){
-                studentGrade.letterGrade = "C+";
-            }
-            else if (studentGrade["percent"] >= 73){
-                studentGrade.letterGrade = "C";
-            }
-            else if (studentGrade["percent"] >= 70){
-                studentGrade.letterGrade = "C-";
-            }
-            else if (studentGrade["percent"] >= 67){
-                studentGrade.letterGrade = "D+";
-            }
-            else if (studentGrade["percent"] >= 63){
-                studentGrade.letterGrade = "D";
-            }
-            else if (studentGrade["percent"] >= 60){
-                studentGrade.letterGrade = "D-";
-            }
-            else if (studentGrade["percent"] < 60){
-                studentGrade.letterGrade = "F";
-            }
+            //calc letter grade
+            if (student.Percent >= 93)
+                student.letterGrade = "A";
+            else if (student.Percent >= 90)
+                student.letterGrade = "A-";
+            else if (student.Percent >= 87)
+                student.letterGrade = "B+";
+            else if (student.Percent >= 83)
+                student.letterGrade = "B";
+            else if (student.Percent >= 80)
+                student.letterGrade = "B-";
+            else if (student.Percent >= 77)
+                student.letterGrade = "C+";
+            else if (student.Percent >= 73)
+                student.letterGrade = "C";
+            else if (student.Percent >= 70)
+                student.letterGrade = "C-";
+            else if (student.Percent >= 67)
+                student.letterGrade = "D+";
+            else if (student.Percent >= 63)
+                student.letterGrade = "D";
+            else if (student.Percent >= 60)
+                student.letterGrade = "D-";
+            else if (student.Percent < 60)
+                student.letterGrade = "F";
 
-            $("#fNameOutput").text("First Name: " + studentGrade["fName"]);
-            $("#lNameOutput").text("Last Name: " + studentGrade["lName"]);
-            $("#percOutput").text("Percent Grade: " + studentGrade["percent"]);
-            $("#letterOutput").text("Letter Grade: " + studentGrade["letterGrade"]);
+            sArray.push(student);
+
+            //run output function
+            outputStudents();
+        }
+
+        //sort by name
+        function sortName(){
+            event.preventDefault();
+            function sortByName(name1,name2) {
+                if(name1.Name < name2.Name)
+                    return -1
+                else if(name1.Name > name2.Name)
+                    return 1;
+                else
+                    return 0;
+            }
+            sArray.sort(sortByName);
+
+            //run output function
+            outputStudents();
+        }
+
+        //sort by grade
+        function sortGrade(){
+            event.preventDefault();
+            function sortByGrade(percent1,percent2) {
+                if(percent1.Percent < percent2.Percent)
+                    return -1
+                else if(percent1.Percent > percent2.Percent)
+                    return 1;
+                else
+                    return 0;
+            }
+            sArray.sort(sortByGrade);
+
+            //run output function
+            outputStudents();
+        }
+
+        //function to output text
+        function outputStudents(){
+            var studentList = ``;
+            for(i=0;i<sArray.length;i++) {
+                studentList += `Name: ${sArray[i].Name}, Percent: ${sArray[i].Percent}, Grade: ${sArray[i].letterGrade}` + '<br>';
+            }
+            $("#output").html(studentList);
         }
     }
 )
